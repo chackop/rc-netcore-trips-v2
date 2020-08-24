6b0e1425a6c2da960681c4bce78d04f11d327f68
Chacko <chackop@gmail.com>
2020-08-24 14:19:52 +0100

    applied create

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
-import './custom.css'
+import './custom.css';
+import { Trips } from './components/Trip/Trips';
+import { Create } from './components/Trip/Create';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route exact path="/" component={Home} />
+        <Route path="/create" component={Create} />
+        <Route path="/trips" component={Trips} />
       </Layout>
     );
   }
