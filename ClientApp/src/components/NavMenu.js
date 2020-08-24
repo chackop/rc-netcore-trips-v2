080a78232abd6aaa3fffe0b5f0c39d56f38838f2
Chacko <chackop@gmail.com>
2020-08-24 16:06:11 +0100

    applied nav menu for logging in out

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -9,59 +9,58 @@ import {
   NavLink,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
 import './NavMenu.css';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = () => {
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
 
-  constructor(props) {
-    super(props);
+  return (
+    <header>
+      <Navbar
+        className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+        light
+      >
+        <Container>
+          <NavbarBrand tag={Link} to="/">
+            Trips
+          </NavbarBrand>
+          {isAuthenticated ? (
+            <ul className="navbar-nav flex-grow">
+              <NavItem>{user.name}</NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/create">
+                  Create
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/trips">
+                  Trips
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <button className="btn btn-danger" onClick={() => logout()}>
+                  Log out
+                </button>
+              </NavItem>
+            </ul>
+          ) : (
+            <ul className="navbar-nav flex-grow">
+              <NavItem>
+                <button
+                  className="btn btn-success"
+                  onClick={() => loginWithRedirect()}
+                >
+                  Log In
+                </button>
+              </NavItem>
+            </ul>
+          )}
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true,
-    };
-  }
-
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
-
-  render() {
-    return (
-      <header>
-        <Navbar
-          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
-          light
-        >
-          <Container>
-            <NavbarBrand tag={Link} to="/">
-              Trips
-            </NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse
-              className="d-sm-inline-flex flex-sm-row-reverse"
-              isOpen={!this.state.collapsed}
-              navbar
-            >
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/create">
-                    Create
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/trips">
-                    Trips
-                  </NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
-}
+export default NavMenu;
